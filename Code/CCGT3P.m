7a2c87b9dc6fe6e819130138f8843abe70192e53
Son <Son.tran.be@gmail.com>
2017-12-13 18:40:04 +0100

    hhmm....

--- Code/CCGT3P.m
+++ Code/CCGT3P.m
@@ -121,7 +121,7 @@ table(1,4) = XSteam('s_pT',table(1,1),table(1,2));
 table(1,5) = XSteam('v_pT',table(1,2),table(1,2));
 table(1,6) = table(1,3)-h0_273 - (T_0+273.15)*(table(1,4)-s0_273); 
 %% AT THE EXIT OF THE MAIN PUMP
-table(2,1) = plow; 
+table(2,1) = pdrum; 
     h2s = XSteam('h_ps',table(2,1),table(1,4));
 table(2,3) = 1/eta_SiC * h2s + (1-1/eta_SiC)*table(1,3);    
 table(2,2) = XSteam('T_ph',table(2,1),table(2,3));
@@ -129,7 +129,7 @@ table(2,4) = XSteam('s_ph',table(2,1),table(2,3));
 table(2,5) = XSteam('v_ph',table(2,1),table(2,3));
 table(2,6) = table(2,3)-h0_273 - (T_0+273.15)*(table(2,4)-s0_273); 
 WmC = table(2,3) - table(1,3);
-%% Mass flow
+%% Massflow
 k_hp = 0.8;
 k_ip = 0.1;
 if abs(k_hp+k_ip) > 1
@@ -141,8 +141,79 @@ mdot_w = P_e/(Wm*10^3 /(eta_mec));
 mdot_w_HP = k_hp*mdot_w; % First Guess
 mdot_w_IP = k_ip*mdot_w;
 mdot_w_LP = (1-k_hp-k_ip)*mdot_w;
+%% THE CHIMNEY (OH NO ! NOT THE CHIMNEY !)
+% 8 AFTER ECO LP
+table(8,1) = table(2,1);
+table(8,2) = XSteam('Tsat_p',pdrum);
+table(8,3) = XSteam('hL_p',table(8,1));
+table(8,4) = XSteam('sL_p',table(8,1));
+table(8,5) = XSteam('vL_p',table(8,1));
+table(8,6) = table(8,3)-h0_273 - (T_0+273.15)*(table(8,4)-s0_273); 
+% 9 AFTER LP BALLON LIQUID + PUMP
+table(9,1) = pmid;
+    h9s = XSteam('h_ps',table(9,1),table(8,4));
+table(9,3) = 1/eta_SiC * h9s + (1-1/eta_SiC)*table(8,3);
+table(9,2) = XSteam('T_ph',table(9,1),table(9,3));
+table(9,4) = XSteam('s_pt',table(9,1),table(9,2));
+table(9,5) = XSteam('v_pt',table(9,1),table(9,2));
+table(9,6) = table(9,3)-h0_273 - (T_0+273.15)*(table(9,4)-s0_273);
+WmC2 = abs(table(9,3) - XSteam('HL_p',plow));
+% 10 AFTER LP BALLON VAPOR
+table(10,1) = plow;
+table(10,2) = XSteam('Tsat_p',plow);
+table(10,3) = XSteam('hV_p',table(10,1));
+table(10,4) = XSteam('sV_p',table(10,1));
+table(10,5) = XSteam('vV_p',table(10,1));
+table(10,6) = table(10,3)-h0_273 - (T_0+273.15)*(table(10,4)-s0_273);
+% 11 AFTER SUP LP
+table(11,1) = plow;
+table(11,2) = XSteam('Tsat_p',pmid);
+table(11,3) = XSteam('h_pt',table(11,1),table(11,2));
+table(11,4) = XSteam('s_pt',table(11,1),table(11,2));
+table(11,5) = XSteam('v_pt',table(11,1),table(11,2));
+table(11,6) = table(10,3)-h0_273 - (T_0+273.15)*(table(10,4)-s0_273);
 
-%% COMBUSTION
+
+% 12 AFTER ECO IP
+table(12,1) = pmid;
+table(12,2) = XSteam('Tsat_p',pmid);
+table(12,3) = XSteam('hL_p',table(12,1));
+table(12,4) = XSteam('sL_p',table(12,1));
+table(12,5) = XSteam('vL_p',table(12,1));
+table(12,6) = table(12,3)-h0_273 - (T_0+273.15)*(table(12,4)-s0_273);
+% 13 AFTER IP BALLON LIQUID + Pump
+table(13,1) = phig;
+    h13s = XSteam('h_ps',table(13,1),table(12,4));
+table(13,3) = 1/eta_SiC * h13s + (1-1/eta_SiC)*table(12,3);
+table(13,2) = XSteam('T_ph',table(13,1),table(13,3));
+table(13,4) = XSteam('s_pt',table(13,1),table(13,2));
+table(13,5) = XSteam('v_pt',table(13,1),table(13,2));
+table(13,6) = table(13,3)-h0_273 - (T_0+273.15)*(table(13,4)-s0_273);
+WmC3 = abs(table(13,3) - XSteam('HL_p',plow));
+% 14 AFTER ECO HP
+table(14,1) = phig;
+table(14,2) = XSteam('Tsat_p',phig);
+table(14,3) = XSteam('hL_p',table(14,1));
+table(14,4) = XSteam('sL_p',table(14,1));
+table(14,5) = XSteam('vL_p',table(14,1));
+table(14,6) = table(14,3)-h0_273 - (T_0+273.15)*(table(14,4)-s0_273);
+% 15 AFTER IP BALLON Vapor
+table(15,1) = pmid;
+table(15,2) = XSteam('Tsat_p',pmid);
+table(15,3) = XSteam('hV_p',table(15,1));
+table(15,4) = XSteam('sV_p',table(15,1));
+table(15,5) = XSteam('vV_p',table(15,1));
+table(15,6) = table(15,3)-h0_273 - (T_0+273.15)*(table(15,4)-s0_273);
+% 16 AFTER HP BALLOON VAPOR 
+table(16,1) = phig;
+table(16,2) = XSteam('Tsat_p',phig);
+table(16,3) = XSteam('hV_p',table(16,1));
+table(16,4) = XSteam('sV_p',table(16,1));
+table(16,5) = XSteam('vV_p',table(16,1));
+table(16,6) = table(16,3)-h0_273 - (T_0+273.15)*(table(16,4)-s0_273);
+
+
+%% Output Massflow
 % MASSFLOW is a vector containing : 
 %   -massflow(1) [kg/s]: massflow of steam at 3 (shortcut high pressure)
 %   -massflow(2) [kg/s]: massflow of steam at 6 (IP)
@@ -167,13 +238,33 @@ if display == 1, visibility = 'on'; , else visibility = 'off'; , end
      end   
      S=[S1,S2];
      T=[T,T];
-    plot(S,T);
+    plot(S,T,'b:');
     for i = 1 : length(table(:,1))
         plot(table(i,4),table(i,2),'b^');
         text(table(i,4)+0.1,table(i,2)+0.1,sprintf('%d',i));
-        if i <= length(table(:,1))-1, plot([table(i,4) table(i+1,4)], [table(i,2) table(i+1,2)]), end
+        if i <= 6 && i ~= 2, plot([table(i,4) table(i+1,4)], [table(i,2) table(i+1,2)],'b'), end
     end
-    plot([table(length(table(:,1)),4) table(1,4)], [table(length(table(:,1)),2) table(1,2)])
+    plot([table(7,4) table(1,4)], [table(7,2) table(1,2)],'b')
+    
+    plot([table(2,4) table(8,4)], [table(2,2) table(8,2)],'b')
+    plot([table(8,4) table(10,4)], [table(8,2) table(10,2)],'r--')
+        plot([table(10,4) table(11,4)], [table(10,2) table(11,2)],'r--')
+        plot([table(11,4) table(6,4)], [table(11,2) table(6,2)],'r--')
+    plot([table(8,4) table(9,4)], [table(8,2) table(9,2)],'g--')
+        plot([table(9,4) table(12,4)], [table(9,2) table(12,2)],'g--')
+        plot([table(12,4) table(13,4)], [table(12,2) table(13,2)],'g--')
+        plot([table(13,4) table(15,4)], [table(13,2) table(15,2)],'g--')
+        plot([table(15,4) table(4,4)], [table(15,2) table(4,2)],'g--')
+    plot([table(12,4) table(14,4)], [table(12,2) table(14,2)],'y--') 
+        plot([table(14,4) table(16,4)], [table(14,2) table(16,2)],'y--')
+        plot([table(16,4) table(3,4)], [table(16,2) table(3,2)],'y--') 
+        
+    h = zeros(3, 1);
+    h(1) = plot(NaN,NaN,'--r');
+    h(2) = plot(NaN,NaN,'--g');
+    h(3) = plot(NaN,NaN,'--y');
+    legend(h,'HP Evolution Part','IP Part Evolution','LP Part Evolution');    
+        
     grid on;
     grid minor;
     hold off;
@@ -189,7 +280,8 @@ if display == 1, visibility = 'on'; , else visibility = 'off'; , end
 % %     disp('Tgas[C]       hgas       p[bar]     T[C]      h[MJ]     s[J/K]        v      x[KJ]');
 % %     disp([table_chim(:,1) table_chim(:,2) table_chim(:,3) table_chim(:,4) table_chim(:,5)/10^3  ]);
 %     
-%     disp('  WmC[KJ]    WmT[KJ]    Wm[KJ]    mdot_g    mdot_w    mdot_w_HP  ');
-%     disp([WmT_HP WmT_LP  Wm mdot_g mdot_w mdot_w_HP]);
+format short g;
+    disp('        STATE       p[bar]         T[C]         h[KJ]     s[KJ/C]            v        e[KJ]');
+    disp([linspace(1,length(table(:,1)),length(table(:,1)))' table(:,1) table(:,2) (table(:,3)) (table(:,4)) table(:,5) table(:,6) ]);
 
 end
